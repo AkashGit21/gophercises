3ce9ea9a931cf135175bded56aa0a5c7d8312b69
Akash Gupta <akashgzb98@gmail.com>
2022-04-09 20:45:10 +0530

    Add BoltDB operations

--- cli-task-manager/main.go
+++ cli-task-manager/main.go
@@ -1,9 +1,30 @@
 package main
 
 import (
+	"fmt"
+	"os"
+	"path/filepath"
+
 	"github.com/AkashGit21/gophercises/cli-task-manager/cmd"
+	"github.com/AkashGit21/gophercises/cli-task-manager/db"
+	homedir "github.com/mitchellh/go-homedir"
 )
 
 func main() {
+
+	home, err := homedir.Dir()
+	if err != nil {
+		panic(err)
+	}
+	dbPath := filepath.Join(home, "tasks.db")
+	must(db.Init(dbPath))
+
 	cmd.Execute()
+}
+
+func must(err error) {
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
 }
